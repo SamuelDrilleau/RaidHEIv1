63981d6d75a0033621498d0bbd462609bafc9747
BuildTools <samuel.drilleau@hei.yncrea.fr>
2018-03-23 21:12:21 +0100

    fix index

--- src/main/java/servlets/dao/impl/DataSourceProvider.java
+++ src/main/java/servlets/dao/impl/DataSourceProvider.java
@@ -12,11 +12,11 @@ public class DataSourceProvider {
     public static DataSource getDataSource() {
         if (dataSource == null) {
             dataSource = new MysqlDataSource();
-            dataSource.setServerName("localhost");
+            dataSource.setServerName("u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
             dataSource.setPort(3306);
-            dataSource.setDatabaseName("raidhei");
-            dataSource.setUser("root");
-            dataSource.setPassword("root");
+            dataSource.setDatabaseName("cgy7wi5ylgn0w04a");
+            dataSource.setUser("jhahzr1gs6wfv6w4");
+            dataSource.setPassword("s32ef8rlof1neyk4");
     }
 		return dataSource;
     }
