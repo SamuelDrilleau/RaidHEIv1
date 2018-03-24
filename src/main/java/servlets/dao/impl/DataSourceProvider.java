0b720997d70d17239b91275136ec547e34497e0a
BuildTools <samuel.drilleau@hei.yncrea.fr>
2018-03-24 16:09:41 +0100

    maj info co db

--- src/main/java/servlets/dao/impl/DataSourceProvider.java
+++ src/main/java/servlets/dao/impl/DataSourceProvider.java
@@ -15,8 +15,8 @@ public class DataSourceProvider {
             dataSource.setServerName("u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
             dataSource.setPort(3306);
             dataSource.setDatabaseName("cgy7wi5ylgn0w04a");
-            dataSource.setUser("jhahzr1gs6wfv6w4");
-            dataSource.setPassword("s32ef8rlof1neyk4");
+            dataSource.setUser("peayqn98emob8vni");
+            dataSource.setPassword("vqan4nxqkb38zwb6");
     }
 		return dataSource;
     }
