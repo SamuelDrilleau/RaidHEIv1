4dc3e756e3f9ce40839dc8efeac6dc16b2020d95
lamaudbis <32640944+lamaudbis@users.noreply.github.com>
2018-04-15 19:33:51 +0200

    Update PhotoDaoImpl.java

--- src/main/java/servlets/dao/impl/PhotoDaoImpl.java
+++ src/main/java/servlets/dao/impl/PhotoDaoImpl.java
@@ -8,31 +8,43 @@ import servlets.model.Photo;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class PhotoDaoImpl implements PhotoDao{
+public class PhotoDaoImpl implements PhotoDao {
 
 
     @Override
-    public void addPhoto(Photo photo) {
-        String query ="INSERT INTO photo(annee,nom) VALUES(?,?)";
+        /* Cette fonction permet d'ajouter une photo à la base de données */
+    public Photo addPhoto(Photo photo) {
+        String query = "INSERT INTO photo(id,annee,nom) VALUES(?,?,?)";
         try {
             Connection connection = DataSourceProvider.getDataSource().getConnection();
-            PreparedStatement stmt = connection.prepareStatement(query);
-            stmt.setInt(1,photo.getAnnee());
-            stmt.setString(2,photo.getNom());
+            PreparedStatement stmt = connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, photo.getId());
+            stmt.setInt(2, photo.getAnnee());
+            stmt.setString(3, photo.getNom());
             stmt.executeUpdate();
+
+            try(ResultSet ids =stmt.getGeneratedKeys()){
+                if(ids.next()){
+                    int generateId = ids.getInt(1);
+                    photo.setId(generateId);
+                }
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     @Override
+        /* Cette fonction permet de récupérer les photos de la base de données */
+
     public ArrayList<Photo> getAllPhoto(int valeur) {
         ArrayList<Photo> listPhoto = new ArrayList<>();
         try {
             Connection connection = DataSourceProvider.getDataSource().getConnection();
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM photo WHERE annee=valeur");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM photo");
             while (rs.next()) {
                 Photo photo = new Photo(
                         rs.getInt("id"),
