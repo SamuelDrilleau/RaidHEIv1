0cdc0ff52213ca5fce2b33658f96cadc5454e252
lamaudbis <32640944+lamaudbis@users.noreply.github.com>
2018-04-15 19:03:46 +0200

    Update DeconnexionServlet.java

--- src/main/java/servlets/servlets/DeconnexionServlet.java
+++ src/main/java/servlets/servlets/DeconnexionServlet.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 public class DeconnexionServlet extends HttpServlet {
 
     @Override
+
+   /*Page de déconnexion*/
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getSession().removeAttribute("utilisateurConnecte");
         resp.sendRedirect("connexion");
